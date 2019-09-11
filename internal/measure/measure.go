e1880abe7be50f870c7eea63b5af5d61496e49fa
jsign <jsign.uy@gmail.com>
2019-09-11 20:46:59 -0300

    internal/measure: goroutine clean cancelation on error & refactoring

--- internal/measure/measure.go
+++ internal/measure/measure.go
@@ -1,6 +1,7 @@
 package measure
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -27,23 +28,13 @@ var (
 // Measure latencies for reqs and return results
 func Measure(reqs []http.Request, perReqCount int, concurrency int) ([][]int64, error) {
 	reqLatencies := make([][]int64, len(reqs))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	workerRes := make(chan [][]int64, concurrency)
 	errChan := make(chan error)
 	for i := 0; i < concurrency; i++ {
-		go func() {
-			data := make([][]int64, len(reqs))
-			for i := range reqs {
-				latencies, err := measureLatencies(&reqs[i], perReqCount/concurrency)
-				if err != nil {
-					errChan <- err
-					return
-				}
-				data[i] = latencies
-			}
-
-			workerRes <- data
-		}()
+		go workerMeasure(ctx, reqs, workerRes, errChan, perReqCount/concurrency)
 	}
 
 	for i := 0; i < concurrency; i++ {
@@ -53,11 +44,32 @@ func Measure(reqs []http.Request, perReqCount int, concurrency int) ([][]int64, 
 				reqLatencies[i] = append(reqLatencies[i], newData[i]...)
 			}
 		case err := <-errChan:
+			cancel()
 			return [][]int64{}, fmt.Errorf("error while measuring latency in request %d: %w", i, err)
 		}
 	}
 
 	return reqLatencies, nil
+}
+
+func workerMeasure(ctx context.Context, reqs []http.Request, resChan chan<- [][]int64, errChan chan<- error, count int) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+	}
+
+	data := make([][]int64, len(reqs))
+	for i := range reqs {
+		latencies, err := measureLatencies(&reqs[i], count)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		data[i] = latencies
+	}
+
+	resChan <- data
 }
 
 func measureLatencies(r *http.Request, count int) ([]int64, error) {
